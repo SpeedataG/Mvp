935e1cebb7dbf1ea5e43501fad8e4c2be3197e47
Reginer <282921012@qq.com>
2019-10-29 13:21:10 +0800

    add RxJava2

--- mvp/src/main/java/com/spd/lib/mvp/BasePresenter.java
+++ mvp/src/main/java/com/spd/lib/mvp/BasePresenter.java
@@ -3,21 +3,33 @@ package com.spd.lib.mvp;
 import java.lang.ref.Reference;
 import java.lang.ref.WeakReference;
 
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
 
 /**
  * @author :Reginer in  2017/9/18 13:14.
- *         联系方式:QQ:282921012
- *         功能描述:Presenter基类
+ * 联系方式:QQ:282921012
+ * 功能描述:Presenter基类
  */
-public abstract class BasePresenter<V,M extends BaseModel> {
+public abstract class BasePresenter<V, M extends BaseModel> {
     protected M mModel;
-    public BasePresenter(){
+
+    public BasePresenter() {
         mModel = createModel();
     }
+
     private Reference<V> mViewRef;
+
+    private CompositeDisposable mDisposables;
 
     void attachView(V view) {
         mViewRef = new WeakReference<>(view);
+        mDisposables = new CompositeDisposable();
+    }
+
+    public void addDisposable(Disposable disposable) {
+        mDisposables.add(disposable);
     }
 
     public V getView() {
@@ -37,10 +49,12 @@ public abstract class BasePresenter<V,M extends BaseModel> {
             mViewRef.clear();
             mViewRef = null;
         }
+        mDisposables.clear();
     }
 
     /**
      * 创建model
+     *
      * @return model
      */
     protected abstract M createModel();
